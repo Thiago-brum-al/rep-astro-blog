1061824958311e43e7d022959142919e6748c78a
Thiago Brum <thiagobrum222@gmail.com>
2026-09-01 19:24:05 -0300

    add markdown-it and sanitize-html for content processing in RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
+import MarkdownIt from "markdown-it";
+import sanitize from "sanitize-html";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
 
@@ -15,13 +19,30 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#site
     site: site!,
+
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: posts.map(({data, id}) => ({ 
+    items: posts.map(({data, id, body}) => ({ 
         title: data.title,
         pubDate: data.date,
         description: data.description,
-        link: `/posts/${id}`
+        link: `/posts/${id}`,
+
+
+        content: sanitize(parser.render(body!), {
+          allowedTags: sanitize.defaults.allowedTags.concat(['img']),
+        }),
+
+        customData: `<media:content
+            type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+            width="${data.image.width}"
+            height="${data.image.height}"
+            medium="image"
+            url="${site + data.image.src}" />
+        `,
      })),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
