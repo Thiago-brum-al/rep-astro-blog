0e66d4f086dadfeb392a46284683676bdd825c6d
Thiago Brum <thiagobrum222@gmail.com>
2026-09-01 08:45:11 -0300

    update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   vite: {plugins: [tailwindcss()]},
   integrations: [mdx()],
-  site: 'http://example.com'
+  site: 'https://6a96b9ddf63f37b6ae0a330d--blog-astro-test01.netlify.app/'
 });
